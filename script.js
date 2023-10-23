b98a46de21cc0704a21f551f79f9d275266c8f05
mmm <proweb2008.m@gmail.com>
2023-10-23 14:13:05 +0500

    mmm

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name pl !")
+let a = + prompt("Year")
+let b = +prompt("Your age !")
+function math(a, b) {
+  return a - b
+}
+alert("HI " + name + " " + math(a, b))
+console.log("HI " + name + " " + math(a, b));
